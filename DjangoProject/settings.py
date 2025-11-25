6f3217ac17b2363c34f6186491407d130cb16828
mosanNefilibata <sinister_heart@outlook.com>
2025-11-25 22:55:22 +0800

    优化按钮样式

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BlindBoxes',
+        'USER': 'root',
+        'PASSWORD': 'Lcy20060118',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
